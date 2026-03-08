850c8bdb2693b3ecbe1db8c0437a3fe99d781ce2
hiplitewhat <hiplitewhat@gmail.com>
2026-03-09 02:15:50 +0800

    Update Template.kt

--- plugins/Template/src/main/kotlin/com/aliucord/plugins/template/Template.kt
+++ plugins/Template/src/main/kotlin/com/aliucord/plugins/template/Template.kt
@@ -1,14 +1,44 @@
-package com.aliucord.plugins.template
+package com.example;
 
-import android.content.Context
-import com.aliucord.annotations.AliucordPlugin
-import com.aliucord.entities.Plugin
+import com.aliucord.entities.Plugin;
+import com.aliucord.patcher.Hook;
+import com.aliucord.patcher.Patcher;
 
-@AliucordPlugin(requiresRestart = false)
-@Suppress("unused")
-class Template : Plugin() {
-    override fun start(context: Context) {}
-    override fun stop(context: Context) {
-        patcher.unpatchAll()
+import com.discord.models.message.Message;
+
+public class AdvancedPlugin extends Plugin {
+
+    @Override
+    public void start() throws Throwable {
+
+        logger.info("Advanced Plugin Started");
+
+        // Message hook
+        Patcher.addPatch(
+            Message.class.getDeclaredMethod("getContent"),
+            new Hook(param -> {
+
+                Object result = param.getResult();
+
+                if (result instanceof String) {
+                    String content = (String) result;
+
+                    if (content.contains("hello")) {
+                        logger.info("Detected hello message");
+                    }
+                }
+
+                return null;
+            })
+        );
+
+    }
+
+    @Override
+    public void stop() {
+
+        Patcher.unpatchAll();
+        logger.info("Advanced Plugin Stopped");
+
     }
 }
